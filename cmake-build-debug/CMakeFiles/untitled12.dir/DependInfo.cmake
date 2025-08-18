9de9fc31ef0b70abbc463f4dd278dfab7a366c21
AbbosZaitov04 <78434226+AbbosZaitov04@users.noreply.github.com>
2025-08-18 09:52:46 +0500

    Abbos Second Commit

---
+++ cmake-build-debug/CMakeFiles/untitled12.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CProjectUEnter/Address.cpp" "CMakeFiles/untitled12.dir/Address.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Address.cpp.obj.d"
+  "D:/CProjectUEnter/Cancel.cpp" "CMakeFiles/untitled12.dir/Cancel.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Cancel.cpp.obj.d"
+  "D:/CProjectUEnter/CheckOut.cpp" "CMakeFiles/untitled12.dir/CheckOut.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/CheckOut.cpp.obj.d"
+  "D:/CProjectUEnter/Hotel.cpp" "CMakeFiles/untitled12.dir/Hotel.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Hotel.cpp.obj.d"
+  "D:/CProjectUEnter/Menu.cpp" "CMakeFiles/untitled12.dir/Menu.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Menu.cpp.obj.d"
+  "D:/CProjectUEnter/Person.cpp" "CMakeFiles/untitled12.dir/Person.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Person.cpp.obj.d"
+  "D:/CProjectUEnter/Rooms.cpp" "CMakeFiles/untitled12.dir/Rooms.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Rooms.cpp.obj.d"
+  "D:/CProjectUEnter/Search.cpp" "CMakeFiles/untitled12.dir/Search.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Search.cpp.obj.d"
+  "D:/CProjectUEnter/Search2.cpp" "CMakeFiles/untitled12.dir/Search2.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Search2.cpp.obj.d"
+  "D:/CProjectUEnter/Service.cpp" "CMakeFiles/untitled12.dir/Service.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/Service.cpp.obj.d"
+  "D:/CProjectUEnter/main.cpp" "CMakeFiles/untitled12.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled12.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
